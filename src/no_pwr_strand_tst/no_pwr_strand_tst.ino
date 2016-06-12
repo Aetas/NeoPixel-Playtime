da66bf3f5fad58155d90e2c1890acf7f59594407
Derek Prince <ace.prince08@gmail.com>
2016-06-12 14:10:14 -0600

    Fun fact, the string test doesn't compile with double static void method declarations.

--- src/no_pwr_strand_tst/no_pwr_strand_tst.ino
+++ src/no_pwr_strand_tst/no_pwr_strand_tst.ino
@@ -12,7 +12,7 @@
 #include <Adafruit_NeoPixel.h>
  
 #define PIN      6
-#define N_LEDS 144
+#define N_LEDS 60
  
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
  
@@ -26,8 +26,8 @@ void loop() {
   chase(strip.Color(0, 0, 255)); // Blue
 }
  
-static void chase(uint32_t c) {
-  for(uint16_t i=0; i<strip.numPixels()+4; i++) {
+void chase(uint32_t c) {
+  for(uint16_t i=0; i < strip.numPixels()+4; i++) {
       strip.setPixelColor(i  , c); // Draw new pixel
       strip.setPixelColor(i-4, 0); // Erase pixel a few steps back
       strip.show();
