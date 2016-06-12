e2d887aa73cab0b10977d56354cb2851a8518d53
Derek Prince <ace.prince08@gmail.com>
2016-06-12 14:45:30 -0600

    Just some setup stuff. Nothing ready. I'm not honestly sure why I'm even committing this.

--- src/neopixel_learnin/neopixel_learnin.ino
+++ src/neopixel_learnin/neopixel_learnin.ino
@@ -1,7 +1,12 @@
 #include <Adafruit_NeoPixel.h>
-void setup() {
-  // put your setup code here, to run once:
 
+#define PIN 6     // dig. PWM out pin
+#define N_LED 60  // number of LEDs per strand. (0-59)
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800); //initialize
+
+void setup() {
+  strip.begin();  //Thrilling.
 }
 
 void loop() {
